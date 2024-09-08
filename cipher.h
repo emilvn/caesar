2e6c851b2e1d8e452361dc43a5d4d00ee20669f5
emilvn <emilvnielsen@hotmail.com>
2024-09-08 11:26:06 +0200

    initial

---
+++ cipher.h
@@ -0,0 +1,7 @@
+int letterToNumber(char);
+char numberToLetter(int);
+int shift(int number, int shiftvalue);
+void encrypt(char* text, int shift);
+void decrypt(char* text, int shift);
+void init(char* holder[]);
+int len(char*);
